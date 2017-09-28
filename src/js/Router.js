5ad768eced5e78674703fa1c98f765e37b1361c1
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-28 20:33:06 +1000

    Added history package / public and private routes

--- src/js/Router.js
+++ src/js/Router.js
@@ -1,22 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    HashRouter
-} from 'react-router-dom';
+import { Route, HashRouter, Redirect } from 'react-router-dom';
+
+import createHistory from 'history/createBrowserHistory';
+const history = createHistory();
 
 import App from './App';
 import Login from './Login';
+import AddPalette from './AddPalette';
+
+import firebase from './firebase';
+import { Loading } from './Ui';
 
 class AppRouter extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            'anonymous_user': null
+        };
+
+        this.loginAdminUser = this.loginAdminUser.bind(this);
+    }
+
+    componentWillMount() {
+        const _this = this;
+
+        if (firebase.auth().currentUser == null) {
+            firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+                .then(function() {
+                    firebase.auth().signInAnonymously().catch(function(error) {
+                        let errorMessage = error.message;
+                        console.log(error);
+                    });
+                })
+                .catch(function(error) {
+                    let errorMessage = error.message;
+                    console.log(error);
+                });
+            // Testing only, this can be removed for production
+            // Serves no purpose
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    _this.setState({
+                        'anonymous_user': user
+                    });
+                }
+            });
+        }
+    }
+
+    loginAdminUser(email, password) {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(function() {
+                firebase.auth().signInWithEmailAndPassword(email, password)
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
     render() {
+        
+        if (firebase.auth().currentUser == null) {
+            return (
+                <Loading />
+            )
+        }
+
         return (
             <HashRouter>
                 <div>
-                    <Route exact path="/" component={App} />
-                    <Route path="/login" component={Login} />
+                    <Route exact path="/" render={() => ( 
+                        <App />
+                    )} />
+                    <Route path="/login" render={() => ( 
+                        firebase.auth().currentUser.isAnonymous === false ? (
+                             <Redirect to="/new" />
+                        ) : (
+                            <Login loginUser={this.loginAdminUser} />
+                        )
+                    )} />
+                    <Route path="/new" render={() => (
+                        firebase.auth().currentUser.isAnonymous === false ? (
+                            <AddPalette />
+                        ) : (
+                            <Redirect to="/" />
+                        )
+                    )} />
                 </div>
             </HashRouter>
         )
