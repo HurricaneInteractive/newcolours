698968773897edb7d3f4f75718cbe9c368885347
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-28 20:33:20 +1000

    Basic structure

---
+++ src/js/AddPalette.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import firebase from './firebase';
+
+class AddPalette extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div class="app-container">
+                <h2>Add new</h2>
+            </div>
+        );
+    }
+}
+
+export default AddPalette;
