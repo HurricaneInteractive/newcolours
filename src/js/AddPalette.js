077f8dd86d59555c72c3690f72220dfd2efb1d9a
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-10-01 19:23:10 +1100

    Add Palette component update

--- src/js/AddPalette.js
+++ src/js/AddPalette.js
@@ -2,21 +2,53 @@ import React from 'react';
 import firebase from './firebase';
 
 class AddPalette extends React.Component {
+    
     constructor() {
         super();
+        this.state = {
+            name: '',
+            hex1: '',
+            hex2: '',
+            hex3: '',
+            hex4: ''
+        };
+
+        this.addNew = this.addNew.bind(this);
+    }
+
+    onInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    generateHexInputs(hexs) {
+        return hexs.map((name, index) => {
+            return (
+                <div className="hex-preview-wrapper" key={index}>
+                    <div className="hex-preview" style={{ 'backgroundColor': this.state[name] }}/>
+                    <input name={name} placeholder="#000" onChange={(e) => { this.onInputChange(e) }} value={this.state[name]} />
+                </div>
+            )
+        });
+    }
+
+    addNew(e) {
+        e.preventDefault();
+        document.getElementById('submit-btn').value = 'Processing...';
+        let hex_group = [this.state.hex1, this.state.hex2, this.state.hex3, this.state.hex4];
+        this.props.createNew(this.state.name, hex_group);
     }
 
     render() {
+        let names = ['hex1', 'hex2', 'hex3', 'hex4'];
         return(
             <div id="add-new" class="app-container">
                 <div class="new-modal">
                     <h2>Add new</h2>
-                    <form>
-                        <input name="name" placeholder="Palette Name" />
-                        <input name="hex1" placeholder="hex code" />
-                        <input name="hex2" placeholder="hex code" />
-                        <input name="hex3" placeholder="hex code" />
-                        <input name="hex4" placeholder="hex code" />
+                    <form onSubmit={(e) => { this.addNew(e) }} >
+                        <input name="name" placeholder="Palette Name" value={this.state.name} onChange={(e) => { this.onInputChange(e) }} />
+                        { this.generateHexInputs(names) }
                         <input type="submit" value="Create" id="submit-btn" />
                     </form>
                 </div>
